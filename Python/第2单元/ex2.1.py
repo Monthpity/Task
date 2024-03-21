3aa392eafa386ea4fa7585c5030b58ef58492e94
Moon <74457835+Monthpity@users.noreply.github.com>
2024-03-21 12:32:55 +0800

    Delete Python/第2单元 directory

--- Python/第2单元/ex2.1.py
+++
@@ -1,7 +0,0 @@
-#IF语句示例
-a=input("请输入第一个数")
-b=input("请输入第二个数")
-if a > b:
-     print('a>b')
-else:
-     print('a<b')
--- Python/第2单元/ex2.4.py
+++
@@ -1,6 +0,0 @@
-#复数类型测试
-f1=4.5+3j
-print(f1)
-print(type(f1))
-print(f1.real)
-print(f1.imag)
